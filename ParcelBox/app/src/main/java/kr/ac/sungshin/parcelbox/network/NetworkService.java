6934814cca6ff09241085591e89738c5da2b0ba6
DESKTOP-V0ENI3K\LG <jieun3001@gmail.com>
2018-01-13 23:47:38 +0900

    join 레트로핏 아이디 중보체크, 이메일인증 완료

--- ParcelBox/app/src/main/java/kr/ac/sungshin/parcelbox/network/NetworkService.java
+++ ParcelBox/app/src/main/java/kr/ac/sungshin/parcelbox/network/NetworkService.java
@@ -1,10 +1,15 @@
 package kr.ac.sungshin.parcelbox.network;
 
 import kr.ac.sungshin.parcelbox.model.request.Login;
+import kr.ac.sungshin.parcelbox.model.response.Result;
 import kr.ac.sungshin.parcelbox.model.response.User;
+import kr.ac.sungshin.parcelbox.model.response.VerificationCodeResult;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /**
  * Created by hyeona on 2018. 1. 9..
@@ -13,4 +18,12 @@ import retrofit2.http.POST;
 public interface NetworkService {
     @POST("/login/login")
     Call<User> getLoginResult(@Body Login login);
+
+    @GET("/join/duplicateCheck/{tempEmail}")
+    Call<Result> getIdCheck(@Query("tempEmail") String id);
+
+    @GET("/join/verificationCode/{memberId}")
+    Call<VerificationCodeResult> getChecknumber (@Query("tempEmail") String id);
+
+
 }
